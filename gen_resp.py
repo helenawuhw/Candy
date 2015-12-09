dc9ce07f2a1fd4656d85a932f3c74dd5b886acde
keotsei <keo34@cornell.edu>
2015-12-09 18:48:07 -0500

    added stemming

--- gen_resp.py
+++ gen_resp.py
@@ -6,9 +6,11 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from LUT import LUTindex
 from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import *
 
 import os
 import sys
+stemmer = PorterStemmer()
 
 readfromfile = "input.txt"
 writetofile = "output.txt"
@@ -16,28 +18,38 @@ writetofile = "output.txt"
 def stopword_filter(words):
     return [w for w in words if w.lower() not in stopwords.words("english")]
 
+
+def stem_filter(words):
+    return [stemmer.stem(w) for w in words if stemmer.stem(w) != ' ']
+
 #To change the output, you need to modify this function
 #This creates the vectors that will be use to compare terms
 def tokenize_text(text):
     #Tokenizes words
     line = [word for sent in sent_tokenize(text) for word in word_tokenize(sent)]
-    
+    line = stem_filter(line)
     #Removes punctuation
     line = [w.lower() for w in line if w.isalpha()]
     tags = nltk.pos_tag(line)
     index = 0
     for (curword, tag) in tags:
         #This makes negative adjectives negated
+        str_negate = ['not','no', 'never']
         if tag == 'JJ':
-            str_negate = ['not','no']
             for neg_index in xrange(len(str_negate)):
                 if str_negate[neg_index] in line[:index]:
                     line[index] = str_negate[neg_index] + ' ' + line[index]
                     del line[line.index(str_negate[neg_index])]
                     index -= 1
-        
+        elif tag == 'VB':
+            for neg_index in xrange(len(str_negate)):
+                if str_negate[neg_index] in line[:index]:
+                    line[index] = str_negate[neg_index] + ' ' + line[index]
+                    del line[line.index(str_negate[neg_index])]
+                    index -= 1
+                            
         index+=1
-    line_priority = [0.5]*len(line)
+    line_priority = [0.45]*len(line)
     new_addition = stopword_filter(line)
     new_addition_priority = [1]*len(new_addition)
     return zip(line, line_priority) + zip(new_addition, new_addition_priority) 
@@ -54,18 +66,23 @@ class GenResp(Thread):
         Thread.__init__(self)
         self.findresp = findresp
         self.current_text = '';
+        self.time_out = 0
 
     def run(self):
         #always runs in the background
-        while True:
-            self.current_text = self.findresp.process_text(self.current_text)
+        while self.time_out < 100:
+            new_text = self.findresp.process_text(self.current_text)
+            if self.current_text == new_text:
+                self.time_out += 1
+            else:
+                self.time_out = 0
+            self.current_text = new_text
             time.sleep(5.0)
 
 class FindResp(object):
     def __init__(self, dictionary):
         self.dictionary = dictionary
         self.monitor_write = Lock()
-
 
     def process_text(self, oldtext):
         
@@ -76,13 +93,15 @@ class FindResp(object):
         if oldtext == text:
             return oldtext
 
-        print text
         line = tokenize_text(text)        
         keys = self.dictionary.get_key_elements()
-        
+        print line
+
         #Sets up default okay response. Will return "OK" if none seem to match
         max_sim = 0.05
         max_key = keys[0]
+        keys.sort()
+
         for cur_key in keys[1:]:
             tokenwords = zip(*tokenize_text(cur_key))[0]
             cur_sim = cosine_similarity(line, tokenwords)
@@ -102,8 +121,6 @@ class FindResp(object):
             while update == 0:
                 try:
                     mturk = filewrite.write(text)
-                    #Notify a change
-                    #print "A CHANGE TO OUTPUT HAS BEEN MADE!"
                     update = 1
                 except MyError as e:
                     print 'waiting on file'
